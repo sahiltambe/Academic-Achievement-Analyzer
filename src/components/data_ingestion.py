ef5eaa0aa3de4ac7b1ef2b3835969c58febd348a
Sahil Tambe <sahiltambe1996@gmail.com>
2024-04-24 18:37:55 +0530

    Model creation with hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,8 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 # Any input data will be passed to this class
 # It will read the data from the source
@@ -50,8 +50,6 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
-
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
@@ -65,4 +63,7 @@ if __name__=="__main__":
     train_data, test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr,_))
